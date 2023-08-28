a120b9f73cf1ff569b71315b78b4841977356ee6
Razvan-BT <spatarurazvan8@gmail.com>
2023-08-28 16:41:06 +0300

    test 3

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it! It a test! test2cxzc2 another dddd 333 dsads test merge" />
+      <HelloWorld msg="You did it! It a test! test2cxzc2 another dddd 333 dsads test merge tert3" />
     </div>
   </header>
 
