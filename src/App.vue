853d89218466cea5762d22d84cca88fd96dd0eba
Razvan-BT <spatarurazvan8@gmail.com>
2023-08-28 15:26:03 +0300

    test 2

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it! It a test! test2cxzc2 another dddd 333" />
+      <HelloWorld msg="You did it! It a test! test2cxzc2 another dddd 333 dsads" />
     </div>
   </header>
 
