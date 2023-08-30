205bf9aa5f5b595b4590a612dd071214a16cd39a
Razvan-BT <spatarurazvan8@gmail.com>
2023-08-30 08:39:52 +0300

    test 3445

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Application test 4" />
+      <HelloWorld msg="Application test 5" />
     </div>
   </header>
 
