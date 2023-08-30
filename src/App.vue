4f1b63b736bc26110ca6aa0f376e4499125bf278
Razvan-BT <spatarurazvan8@gmail.com>
2023-08-30 08:42:45 +0300

    3

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Mergin branch" />
+      <HelloWorld msg="Mergin branch 3" />
     </div>
   </header>
 
